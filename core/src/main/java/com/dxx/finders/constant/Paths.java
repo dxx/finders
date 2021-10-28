2757eebdf928c060dde3e364c9158daf86a4631b
dxx <dx1898@foxmail.com>
2021-10-28 16:53:15 +0800

    Remove useless field

--- core/src/main/java/com/dxx/finders/constant/Paths.java
+++ core/src/main/java/com/dxx/finders/constant/Paths.java
@@ -5,12 +5,6 @@ package com.dxx.finders.constant;
  */
 public class Paths {
 
-    public static final String HELLO = "/hello";
-
-    public static final String HELLO1 = "/hello1";
-
-    public static final String DATA = "/data";
-
     public static final String INSTANCE = "/instance";
 
     public static final String INSTANCE_LIST = INSTANCE + "/list";
