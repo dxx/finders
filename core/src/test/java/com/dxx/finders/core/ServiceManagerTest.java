110981a435979a332655f0e61556308d43dac0a5
dxx <dx1898@foxmail.com>
2021-11-14 11:43:15 +0800

    Add test cases

---
+++ core/src/test/java/com/dxx/finders/core/ServiceManagerTest.java
@@ -0,0 +1,51 @@
+package com.dxx.finders.core;
+
+import com.dxx.finders.cluster.ServerNodeManager;
+import com.dxx.finders.constant.Services;
+import org.junit.jupiter.api.Test;
+
+/**
+ * ServiceManager test.
+ *
+ * @author dxx
+ */
+public class ServiceManagerTest {
+
+    public ServiceManager init() {
+        ServerNodeManager serverNodeManager = ServerNodeManager.init();
+        SyncManager syncManager = new SyncManager(serverNodeManager);
+
+        ServiceManager serviceManager = new ServiceManager(syncManager);
+        syncManager.init(serviceManager);
+        return serviceManager;
+    }
+
+    @Test
+    public void testRegisterInstance() {
+        ServiceManager serviceManager = init();
+
+        Instance instance = new Instance();
+        instance.setServiceName("testService");
+        instance.setCluster(Services.DEFAULT_CLUSTER);
+        instance.setIp("127.0.0.1");
+        instance.setPort(9080);
+
+        serviceManager.registerInstance(Services.DEFAULT_NAMESPACE, instance.getServiceName(), instance);
+    }
+
+    @Test
+    public void testDeregisterInstance() {
+        ServiceManager serviceManager = init();
+
+        Instance instance = new Instance();
+        instance.setServiceName("testService");
+        instance.setCluster(Services.DEFAULT_CLUSTER);
+        instance.setIp("127.0.0.1");
+        instance.setPort(9080);
+
+        serviceManager.registerInstance(Services.DEFAULT_NAMESPACE, instance.getServiceName(), instance);
+
+        serviceManager.deregisterInstance(Services.DEFAULT_NAMESPACE, instance.getServiceName(), instance);
+    }
+
+}
